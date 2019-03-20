c81baaebbd8803cc25e4ffeaadc2986102e5fe2d
Randy Wilson <phoenixrr2113@gmail.com>
2019-03-20 16:05:28 -0400

    created navigation

--- src/hoc/layout/Layout.js
+++ src/hoc/layout/Layout.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import Navigation from '../../components/navigation/Navigation';
+import App from '../../App';
 
 const Layout = () => {
-	return <div>Layout</div>;
+	return (
+		<div>
+			<Navigation />
+			<App />
+		</div>
+	);
 };
 
 export default Layout;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/css/index.css';
-import App from './App';
+// import App from './App';
+import Layout from './hoc/layout/Layout';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Layout />, document.getElementById('root'));
