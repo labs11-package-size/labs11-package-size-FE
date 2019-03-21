28f4f19bbfed554471e6e2cba33faf13cb17fd1c
Randy Wilson <phoenixrr2113@gmail.com>
2019-03-21 00:31:27 -0400

    wired up nav links. created user reducer

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 
@@ -11,7 +12,12 @@ import Layout from './containers/layout/Layout';
 
 import './styles/css/index.css';
 
-const store = createStore(rootReducer, applyMiddleware(thunk, logger));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+	rootReducer,
+	composeEnhancers(applyMiddleware(thunk, logger)),
+);
 
 ReactDOM.render(
 	<Provider store={store}>
