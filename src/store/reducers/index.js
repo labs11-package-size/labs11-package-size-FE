d4dd3e0b361d40fc4307401653cc473dd9f3a97e
Randy Wilson <phoenixrr2113@gmail.com>
2019-03-22 08:27:25 -0400

    changes to redux store.

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,6 +1,10 @@
 import { combineReducers } from 'redux';
 import userReducer from './userReducer';
+import shipmentReducer from './shipmentReducer';
+import productReducer from './productReducer';
 
 export default combineReducers({
 	userReducer,
+	shipmentReducer,
+	productReducer,
 });
