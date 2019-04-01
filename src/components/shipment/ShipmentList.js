8131a22442ceeb831015452327f9ca0c63dd8115
Randy Wilson <phoenixrr2113@gmail.com>
2019-04-01 14:41:54 -0400

    testing

--- src/components/shipment/ShipmentList.js
+++ src/components/shipment/ShipmentList.js
@@ -1,34 +1,11 @@
 import React, { Component } from 'react';
-import { withStyles } from '@material-ui/core/styles';
-import Typography from '@material-ui/core/Typography';
 
 import Shipment from './Shipment';
 
-const styles = {
-	card: {
-		maxWidth: 250,
-		margin: 20,
-	},
-	media: {
-		height: 140,
-	},
-	cardContainer: {
-		display: 'flex',
-		justifyContent: 'center',
-	},
-	mainContainer: {
-		maxWidth: 1100,
-	},
-};
-
 class ShipmentList extends Component {
 	render() {
-		const { classes } = this.props;
 		return (
-			<div className={classes.mainContainer}>
-				<Typography gutterBottom variant="h5" component="h2">
-					Shipments
-				</Typography>
+			<div>
 				{!this.props.shipments ? ( //shipments plural
 					<h5>...loading</h5>
 				) : (
@@ -42,4 +19,4 @@ class ShipmentList extends Component {
 	}
 }
 
-export default withStyles(styles)(ShipmentList);
+export default ShipmentList;
--- src/containers/shipmentView/ShipmentListView.js
+++ src/containers/shipmentView/ShipmentListView.js
@@ -1,15 +1,42 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { withStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
 
 import ShipmentList from '../../components/shipment/ShipmentList';
 import { getShipments } from '../../store/actions/shipmentActions';
+
+const styles = {
+	card: {
+		maxWidth: 250,
+		margin: 20,
+	},
+	media: {
+		height: 140,
+	},
+	cardContainer: {
+		display: 'flex',
+		justifyContent: 'center',
+	},
+	mainContainer: {
+		maxWidth: 1100,
+	},
+};
 
 class ShipmentListView extends Component {
 	componentDidMount() {
 		this.props.getShipments();
 	}
 	render() {
-		return <ShipmentList shipments={this.props.shipments} />;
+		const { classes } = this.props;
+		return (
+			<div className={classes.mainContainer}>
+				<Typography gutterBottom variant="h5" component="h2">
+					Shipments
+				</Typography>
+				<ShipmentList shipments={this.props.shipments} />
+			</div>
+		);
 	}
 }
 
@@ -22,4 +49,4 @@ const mapStateToProps = state => {
 export default connect(
 	mapStateToProps,
 	{ getShipments },
-)(ShipmentListView);
+)(withStyles(styles)(ShipmentListView));
