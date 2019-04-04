f1ff85e784cabf47ae75ef8c1b2f908550c999db
Jacob Bryan <jtb36688@gmail.com>
2019-04-03 23:39:07 -0400

    Packing Component Updates - Added conditionals to differ between mailers and shippers in preview boxes display

--- src/components/packaging/components/Package.js
+++ src/components/packaging/components/Package.js
@@ -3,11 +3,20 @@ import React from "react";
 const Package = props => {
   return (
     <div className="PackageContainer">
+      {(props.box.size === "6 x 6 x 6" || props.box.size === "8 x 6 x 4" || props.box.size === "8 x 8 x 8" || props.box.size === "10 x 8 x 6" || props.box.size === "10 x 10 x 10" || props.box.size === "12 x 6 x 6" || props.box.size === "12 x 12 x 8" || props.box.size === "12 x 12 x 12" || props.box.size === "16 x 12 x 8") ? (
       <img
         className="BoxImage"
         src="https://res.cloudinary.com/https-scannarserver-herokuapp-com/image/upload/c_scale,w_60/v1554325466/UI%20Assets/shipper-kraft-kraft-open.png"
-        alt="thumbnail image of a box"
+        alt="thumbnail image of a shipper box"
+      />) : (
+        <img
+        className="BoxImage"
+        src="https://res.cloudinary.com/https-scannarserver-herokuapp-com/image/upload/c_scale,w_60/v1554348722/UI%20Assets/mailer-glossy-white-open.png"
+        alt="thumbnail image of a mailer box"
       />
+      )}
+      {(props.box.size === "6 x 6 x 6" || props.box.size === "8 x 6 x 4" || props.box.size === "8 x 8 x 8" || props.box.size === "10 x 8 x 6" || props.box.size === "10 x 10 x 10" || props.box.size === "12 x 6 x 6" || props.box.size === "12 x 12 x 8" || props.box.size === "12 x 12 x 12" || props.box.size === "16 x 12 x 8") ? (<p>Shipper-Style Box</p>) : (<p>Mailer-Style Box</p>)}
+
       <p className="PackagedBoxSize">Box Size: {props.box.size}</p>
       <p>Contents:</p>
       <div className="PackagedItemList">
