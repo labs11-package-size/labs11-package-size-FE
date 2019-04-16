c1c08a1d7e79207f6ec586d9bea4310ce6d319c6
Randy Wilson <phoenixrr2113@gmail.com>
2019-04-16 14:15:43 -0400

    image style fixed

--- src/components/product/Product.js
+++ src/components/product/Product.js
@@ -47,6 +47,7 @@ const styles = theme => ({
 	},
 	media: {
 		paddingTop: '56.25%',
+		// width: 100,
 	},
 	actions: {
 		display: 'flex',
@@ -98,10 +99,11 @@ class Product extends Component {
 							title={this.props.product.name}
 						/>
 
-						<img
+						<CardMedia
+							className={classes.media}
 							id={this.props.product.uuid}
-							style={{ width: 100 }}
-							src={this.props.product.thumbnail}
+							// style={{ width: 100 }}
+							image={this.props.product.thumbnail}
 							alt="product"
 						/>
 
